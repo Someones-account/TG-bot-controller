0a705ac42abd56c3b5e213d0c663d61382ab7530
Someones-account <sashko23012006.1@gmail.com>
2026-06-09 00:15:37 +0200

    Enabled database-stored moderation history

--- src/Bot/Moderation.py
+++ src/Bot/Moderation.py
@@ -5,15 +5,18 @@ from telegram import Update, ChatPermissions
 class Moderation:
     def __init__(self, qm):
         self.query_manager = qm
+        self.max_timestamp_obj = datetime(2038, 1, 1, 1, 0, 0)
 
     async def timeout_user(self, update: Update, user_id, chat_id, context):
         try:
+            until_date = datetime.utcnow() + timedelta(minutes=5)
             await context.bot.restrict_chat_member(
                 chat_id=chat_id,
                 user_id=user_id,
                 permissions=ChatPermissions(can_send_messages=False),
-                until_date=datetime.utcnow() + timedelta(minutes=5)
+                until_date=until_date
             )
+            self.__record_action(user_id, "Mute 5min", until_date)
             await update.message.reply_text("Muted for 5 minutes due to spamming.")
             return
         except Exception as e:
@@ -27,10 +30,14 @@ class Moderation:
                 user_id=target_user.id,
                 revoke_messages=False
             )
+            self.__record_action(target_user.id, "Ban", self.max_timestamp_obj)
             await update.message.reply_text(
                 f"{target_user.first_name} has been banned from the group.")
         except Exception as e:
             print(f"Kick failed: {e}")
 
-    def record_action(self):
-        self.query_manager.add_user()
+    def __record_action(self, user_id, action, lift_time):
+        try:
+            self.query_manager.create_entry(user_id, action, lift_time)
+        except Exception as e:
+            print(f"Failed to record action: {e}")
